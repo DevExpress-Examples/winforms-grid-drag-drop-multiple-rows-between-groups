c1196ecfa9073e95c935d26abc7fb89a9b00a63f
DevExpressExampleBot <fakeEmail>
2019-02-12 17:23:32 +0300

    Source auto update [skip ci]

--- VB/Q183557/Form1.vb
+++ VB/Q183557/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -12,90 +11,91 @@ Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Columns
 
 Namespace Q183557
-	Partial Public Class Form1
-		Inherits Form
-		Private dragRectangle As New Rectangle(Point.Empty,SystemInformation.DragSize)
-		Private groupRowHandle As Integer = GridControl.InvalidRowHandle
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
-			Me.carsTableAdapter.Fill(Me.carsDBDataSet.Cars)
+        Private dragRectangle As New Rectangle(Point.Empty,SystemInformation.DragSize)
+        Private groupRowHandle As Integer = GridControl.InvalidRowHandle
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            ' TODO: This line of code loads data into the 'carsDBDataSet.Cars' table. You can move, or remove it, as needed.
+            Me.carsTableAdapter.Fill(Me.carsDBDataSet_Renamed.Cars)
 
-		Private Sub gridView1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles gridView1.MouseDown
-			Dim view As GridView = CType(sender, GridView)
-			If view.GroupCount = 0 Then
-				Return
-			End If
-			Dim hitInfo As GridHitInfo = view.CalcHitInfo(e.Location)
-			If e.Button = MouseButtons.Left AndAlso hitInfo.InRowCell Then
-				dragRectangle.Location = e.Location
-			Else
-				dragRectangle.Location = Point.Empty
-			End If
-		End Sub
+        End Sub
 
-		Private Sub gridView1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles gridView1.MouseMove
-			Dim view As GridView = CType(sender, GridView)
-			If e.Button = MouseButtons.Left AndAlso dragRectangle.Location <> Point.Empty AndAlso (Not dragRectangle.Contains(e.Location)) Then
-				Dim data() As Integer = view.GetSelectedRows()
-				For i As Integer = 0 To data.Length - 1
-					data(i) = view.GetDataSourceRowIndex(data(i))
-				Next i
-				view.GridControl.DoDragDrop(data,DragDropEffects.Move)
-			End If
-		End Sub
+        Private Sub gridView1_MouseDown(ByVal sender As Object, ByVal e As MouseEventArgs) Handles gridView1.MouseDown
+            Dim view As GridView = DirectCast(sender, GridView)
+            If view.GroupCount = 0 Then
+                Return
+            End If
+            Dim hitInfo As GridHitInfo = view.CalcHitInfo(e.Location)
+            If e.Button = MouseButtons.Left AndAlso hitInfo.InRowCell Then
+                dragRectangle.Location = e.Location
+            Else
+                dragRectangle.Location = Point.Empty
+            End If
+        End Sub
 
-		Private Sub gridControl1_DragOver(ByVal sender As Object, ByVal e As DragEventArgs) Handles gridControl1.DragOver
-			Dim control As GridControl = CType(sender, GridControl)
-			Dim view As GridView = CType(control.DefaultView, GridView)
-			Dim info As GridHitInfo = view.CalcHitInfo(control.PointToClient(New Point(e.X, e.Y)))
-			If info.InRowCell Then
-				e.Effect = DragDropEffects.Move
-			Else
-				If info.InRow AndAlso view.IsGroupRow(info.RowHandle) Then
-					groupRowHandle = info.RowHandle
-					timer1.Start()
-				Else
-				e.Effect = DragDropEffects.None
-				End If
-			End If
-		End Sub
+        Private Sub gridView1_MouseMove(ByVal sender As Object, ByVal e As MouseEventArgs) Handles gridView1.MouseMove
+            Dim view As GridView = DirectCast(sender, GridView)
+            If e.Button = MouseButtons.Left AndAlso dragRectangle.Location <> Point.Empty AndAlso Not dragRectangle.Contains(e.Location) Then
+                Dim data() As Integer = view.GetSelectedRows()
+                For i As Integer = 0 To data.Length - 1
+                    data(i) = view.GetDataSourceRowIndex(data(i))
+                Next i
+                view.GridControl.DoDragDrop(data,DragDropEffects.Move)
+            End If
+        End Sub
 
-		Private Sub gridControl1_DragDrop(ByVal sender As Object, ByVal e As DragEventArgs) Handles gridControl1.DragDrop
-			If e.Data IsNot Nothing AndAlso e.Effect = DragDropEffects.Move Then
-				Dim control As GridControl = CType(sender, GridControl)
-				Dim view As GridView = CType(control.DefaultView, GridView)
-				Dim info As GridHitInfo = view.CalcHitInfo(control.PointToClient(New Point(e.X,e.Y)))
-				MoveRows(info.RowHandle,view,CType(e.Data.GetData(GetType(Integer())), Integer()),view.GetRowLevel(info.RowHandle))
-				view.RefreshData()
-			End If
-		End Sub
+        Private Sub gridControl1_DragOver(ByVal sender As Object, ByVal e As DragEventArgs) Handles gridControl1.DragOver
+            Dim control As GridControl = DirectCast(sender, GridControl)
+            Dim view As GridView = CType(control.DefaultView, GridView)
+            Dim info As GridHitInfo = view.CalcHitInfo(control.PointToClient(New Point(e.X, e.Y)))
+            If info.InRowCell Then
+                e.Effect = DragDropEffects.Move
+            Else
+                If info.InRow AndAlso view.IsGroupRow(info.RowHandle) Then
+                    groupRowHandle = info.RowHandle
+                    timer1.Start()
+                Else
+                e.Effect = DragDropEffects.None
+                End If
+            End If
+        End Sub
 
-		Private Sub MoveRows(ByVal target As Integer, ByVal view As GridView, ByVal rows() As Integer, ByVal level As Integer)
-			Dim col As GridColumn = view.GroupedColumns(level - 1)
-			Dim val As Object = view.GetRowCellValue(target,col)
-			For Each row As Integer In rows
-				Dim rowHandle As Integer = view.GetRowHandle(row)
-				view.SetRowCellValue(rowHandle,col,val)
-			Next row
-			If level > 0 Then
-				level -= 1
-				MoveRows(target,view,rows,level)
-			End If
-		End Sub
+        Private Sub gridControl1_DragDrop(ByVal sender As Object, ByVal e As DragEventArgs) Handles gridControl1.DragDrop
+            If e.Data IsNot Nothing AndAlso e.Effect = DragDropEffects.Move Then
+                Dim control As GridControl = DirectCast(sender, GridControl)
+                Dim view As GridView = CType(control.DefaultView, GridView)
+                Dim info As GridHitInfo = view.CalcHitInfo(control.PointToClient(New Point(e.X,e.Y)))
+                MoveRows(info.RowHandle,view,CType(e.Data.GetData(GetType(Integer())), Integer()),view.GetRowLevel(info.RowHandle))
+                view.RefreshData()
+            End If
+        End Sub
 
-		Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles timer1.Tick
-			CType(sender, Timer).Stop()
-			If groupRowHandle <> GridControl.InvalidRowHandle Then
-				gridView1.CollapseAllGroups()
-				gridView1.ExpandGroupRow(groupRowHandle)
-				groupRowHandle = GridControl.InvalidRowHandle
-			End If
-		End Sub
-	End Class
+        Private Sub MoveRows(ByVal target As Integer, ByVal view As GridView, ByVal rows() As Integer, ByVal level As Integer)
+            Dim col As GridColumn = view.GroupedColumns(level - 1)
+            Dim val As Object = view.GetRowCellValue(target,col)
+            For Each row As Integer In rows
+                Dim rowHandle As Integer = view.GetRowHandle(row)
+                view.SetRowCellValue(rowHandle,col,val)
+            Next row
+            If level > 0 Then
+                level -= 1
+                MoveRows(target,view,rows,level)
+            End If
+        End Sub
+
+        Private Sub timer1_Tick(ByVal sender As Object, ByVal e As EventArgs) Handles timer1.Tick
+            DirectCast(sender, Timer).Stop()
+            If groupRowHandle <> GridControl.InvalidRowHandle Then
+                gridView1.CollapseAllGroups()
+                gridView1.ExpandGroupRow(groupRowHandle)
+                groupRowHandle = GridControl.InvalidRowHandle
+            End If
+        End Sub
+    End Class
 End Namespace
---
+++ VB/Q183557/Main.Designer.vb
@@ -0,0 +1,165 @@
+﻿Namespace DXSample
+    Partial Public Class Main
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Windows Form Designer generated code"
+
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.recordBindingSource = New System.Windows.Forms.BindingSource()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.colID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colParentID = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colText = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colInfo = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colValue = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colDt = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colState = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colImage = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.colDetail = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.behaviorManager1 = New DevExpress.Utils.Behaviors.BehaviorManager()
+            Me.dragDropEvents1 = New DevExpress.Utils.DragDrop.DragDropEvents()
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.recordBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.behaviorManager1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.DataSource = Me.recordBindingSource
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(505, 307)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+            ' 
+            ' recordBindingSource
+            ' 
+            Me.recordBindingSource.DataSource = GetType(DXSample.Record)
+            ' 
+            ' gridView1
+            ' 
+            Me.behaviorManager1.SetBehaviors(Me.gridView1, New DevExpress.Utils.Behaviors.Behavior() { (CType(DevExpress.Utils.DragDrop.DragDropBehavior.Create(GetType(DevExpress.XtraGrid.Extensions.ColumnViewDragDropSource), True, True, True, Me.dragDropEvents1), DevExpress.Utils.Behaviors.Behavior))})
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.colID, Me.colParentID, Me.colText, Me.colInfo, Me.colValue, Me.colDt, Me.colState, Me.colImage, Me.colDetail})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.GroupCount = 1
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.OptionsBehavior.EditorShowMode = DevExpress.Utils.EditorShowMode.Click
+            Me.gridView1.OptionsSelection.MultiSelect = True
+            Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() { New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.colParentID, DevExpress.Data.ColumnSortOrder.Ascending)})
+            ' 
+            ' colID
+            ' 
+            Me.colID.FieldName = "ID"
+            Me.colID.Name = "colID"
+            Me.colID.Visible = True
+            Me.colID.VisibleIndex = 0
+            ' 
+            ' colParentID
+            ' 
+            Me.colParentID.FieldName = "ParentID"
+            Me.colParentID.Name = "colParentID"
+            Me.colParentID.Visible = True
+            Me.colParentID.VisibleIndex = 1
+            ' 
+            ' colText
+            ' 
+            Me.colText.FieldName = "Text"
+            Me.colText.Name = "colText"
+            Me.colText.Visible = True
+            Me.colText.VisibleIndex = 1
+            ' 
+            ' colInfo
+            ' 
+            Me.colInfo.FieldName = "Info"
+            Me.colInfo.Name = "colInfo"
+            Me.colInfo.Visible = True
+            Me.colInfo.VisibleIndex = 2
+            ' 
+            ' colValue
+            ' 
+            Me.colValue.FieldName = "Value"
+            Me.colValue.Name = "colValue"
+            Me.colValue.Visible = True
+            Me.colValue.VisibleIndex = 3
+            ' 
+            ' colDt
+            ' 
+            Me.colDt.FieldName = "Dt"
+            Me.colDt.Name = "colDt"
+            Me.colDt.Visible = True
+            Me.colDt.VisibleIndex = 4
+            ' 
+            ' colState
+            ' 
+            Me.colState.FieldName = "State"
+            Me.colState.Name = "colState"
+            Me.colState.Visible = True
+            Me.colState.VisibleIndex = 5
+            ' 
+            ' colImage
+            ' 
+            Me.colImage.FieldName = "Image"
+            Me.colImage.Name = "colImage"
+            ' 
+            ' colDetail
+            ' 
+            Me.colDetail.FieldName = "Detail"
+            Me.colDetail.Name = "colDetail"
+            ' 
+            ' Main
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(505, 307)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Main"
+            Me.Text = "Main"
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.recordBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.behaviorManager1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+
+        End Sub
+
+        #End Region
+
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private recordBindingSource As System.Windows.Forms.BindingSource
+        Private colID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colParentID As DevExpress.XtraGrid.Columns.GridColumn
+        Private colText As DevExpress.XtraGrid.Columns.GridColumn
+        Private colInfo As DevExpress.XtraGrid.Columns.GridColumn
+        Private colValue As DevExpress.XtraGrid.Columns.GridColumn
+        Private colDt As DevExpress.XtraGrid.Columns.GridColumn
+        Private colState As DevExpress.XtraGrid.Columns.GridColumn
+        Private colImage As DevExpress.XtraGrid.Columns.GridColumn
+        Private colDetail As DevExpress.XtraGrid.Columns.GridColumn
+        Private behaviorManager1 As DevExpress.Utils.Behaviors.BehaviorManager
+        Private dragDropEvents1 As DevExpress.Utils.DragDrop.DragDropEvents
+    End Class
+End Namespace
+
